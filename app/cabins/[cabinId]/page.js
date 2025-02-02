6dc0b94315809770cd12b37f620552e7fc7ac2e2
Vaibhav <vaibhavsinha0825@gmail.com>
2025-02-03 03:00:22 +0530

    Implemented filter

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,10 +1,16 @@
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
-import { getCabin } from "../../_lib/data-service";
+import { getCabin, getCabins } from "../../_lib/data-service";
 import Image from "next/image";
+import TextExpander from "../../_components/TextExpander";
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+  return cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
 }
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
@@ -28,7 +34,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
