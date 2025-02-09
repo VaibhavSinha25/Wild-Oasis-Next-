aa1d6abab275c98e6229fd61183e5f965860700d
Vaibhav <vaibhavsinha0825@gmail.com>
2025-02-09 23:13:55 +0530

    Next Auth

--- app/account/page.js
+++ app/account/page.js
@@ -1,6 +1,10 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
-export default function Page() {
-  return <h1>Welcome, Vaibhav</h1>;
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+  return <h1>Welcome, {firstName}</h1>;
 }
