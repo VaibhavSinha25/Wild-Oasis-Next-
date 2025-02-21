c2a23bfcbb534d7e725eee68a7f0e19ba33c0638
Vaibhav <vaibhavsinha0825@gmail.com>
2025-02-22 02:23:01 +0530

    Updating reservation

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { updateGuest } from "../_lib/actions";
-import { useFormStatus } from "react-dom";
+import SubmitButton from "./SubmitButton";
 
 function UpdateProfileForm({ guest, children }) {
   const [count, setCont] = useState();
@@ -54,21 +54,11 @@ function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating profile...">
+          Update Profile
+        </SubmitButton>
       </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
   );
 }
 
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { auth, signIn, signOut } from "./auth";
 import { getBookings } from "./data-service";
 import { supabase } from "./supabase";
@@ -39,6 +40,41 @@ export async function deleteReservation(bookingId) {
 
   if (error) throw new Error("Booking could not be deleted");
   revalidatePath("/account/reservations");
+}
+
+export async function updateBooking(formData) {
+  //1. Authentication
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in ");
+
+  //2. Authorization
+  const bookingId = Number(formData.get("bookingId"));
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((guestBooking) => guestBooking.id);
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to update this booking");
+
+  //3.Mutation
+  const updatedData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+  const { error } = await supabase
+    .from("bookings")
+    .update(updatedData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+  //4. Revalidation
+  revalidatePath(`/account/reservations`);
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  //5. Redirect to path
+  redirect("/account/reservations");
 }
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
